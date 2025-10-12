40a58636fda14ef50927ccf16fe6a2bc489adc2c
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2025-10-12 03:28:26 -0500

    test

--- nummus-game/base_objects/base_level.gd
+++ nummus-game/base_objects/base_level.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-var current_coin: Coin
+var current_hand: Array[Coin]
+@onready var table = $TableHandler
 
 func _ready() -> void:
 	# Inventory.add_item(ObjectManager.create_coin(Constants.COINS.base, Constants.display_type.PLAY))
@@ -8,7 +9,10 @@ func _ready() -> void:
 	ObjectManager.spawn_base_enemy()
 
 func spawn_coin():
-	current_coin = Inventory.inventory[0].duplicate()
-	current_coin.current_state = Constants.display_type.PLAY
-	SceneManager.current_scene.add_child.call_deferred(current_coin)
-	current_coin.position = Vector3(0.367, 0.398, -0.017)
+	var hand_size: int = max(Globals.max_hand, Inventory.inventory.size())
+	for i in range(hand_size):
+		var new_coin: Coin = Inventory.inventory[i].duplicate()
+		current_hand.append(new_coin)
+		new_coin.current_state = Constants.display_type.PLAY
+		SceneManager.current_scene.add_child.call_deferred(new_coin)
+		new_coin.position = table.coin_positions[i]
--- nummus-game/base_objects/table_handler.gd
+++ nummus-game/base_objects/table_handler.gd
@@ -4,15 +4,16 @@ class_name Table
 @onready var markers: Node3D = $Markers
 @onready var endpoint_l: Marker3D = $Markers/EndpointL
 @onready var endpoint_r: Marker3D = $Markers/EndpointR
+@export var coin_positions: Array[Vector3]
 
 var increment: float
 
 func _ready() -> void:
 	print(check_spacing(5))
 
-func check_spacing(hand_size: int) -> Array[Vector3]:
+func check_spacing(hand_size: int):
 	increment = (abs(endpoint_l.position.z) + abs(endpoint_r.position.z))/(hand_size-1)
 	var positions: Array[Vector3] = []
 	for i in range(hand_size):
 		positions.append(endpoint_r.position + Vector3(0, 0, increment*i))
-	return positions
+	coin_positions = positions
