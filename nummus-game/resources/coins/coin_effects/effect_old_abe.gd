1c3597054aef768897e3a581153cc994a7c9f228
Nianjin Yang <nianjindev@gmail.com>
2025-10-17 22:42:15 -0500

    fix: Updated gitignore to ignore addons and fixed old abe

--- nummus-game/resources/coins/coin_effects/effect_old_abe.gd
+++ nummus-game/resources/coins/coin_effects/effect_old_abe.gd
@@ -4,7 +4,6 @@ func effect(stats: Dictionary, side):
 	if side == Sides.HEADS:
 		CommonEffects.do_damage(stats.on_heads.damage)
 func pre_effect(_stats: Dictionary):
-	Globals.head_weight = 1
 	Globals.tail_weight = 0
 func recurring(_stats: Dictionary):
 	pass # Don't change if there is no recurring effect
