0a9152d2dd6eb7f1500cb3a683080d7cce0ca7b4
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2025-11-23 14:59:59 -0600

    feature: more efficient inventory ui

--- nummus-game/base_objects/viewports/inventory_patch_grid.gd
+++ nummus-game/base_objects/viewports/inventory_patch_grid.gd
@@ -3,11 +3,13 @@ extends GridContainer
 const ID_icon = preload(Constants.UI_PATHS.ID_icon)
 @onready var patch: PanelContainer = $"../.."
 var path: Array[Coin]
+var icons = {}
 
 func _ready():
+	icons.clear()
 	GuiManager.update_inventory_patch.connect(update_ui)
-	print(patch.name)
 	set_path()
+	initialize_icons()
 
 func update_ui(type: String):
 	if type == patch.name:
@@ -16,14 +18,20 @@ func update_ui(type: String):
 		
 		for coin in path:
 			var current_instance = ID_icon.instantiate()
-			initialize_icon(coin, current_instance)
+			set_icon(coin, current_instance)
 			add_child(current_instance)
 			
 
-func initialize_icon(coin, current_instance):
-	current_instance.texture = AtlasTexture.new()
-	current_instance.texture.atlas = coin.coin_id.coin_texture
-	current_instance.texture.region = Rect2(2, 0, 15, 15)
+func initialize_icons():
+	for coin in Inventory.inventory:
+		if coin.coin_id.coin_texture not in icons:
+			var texture = AtlasTexture.new()
+			texture.atlas = coin.coin_id.coin_texture
+			texture.region = Rect2(2, 0, 15, 15)
+			icons[coin.coin_id.coin_texture] = texture
+	
+func set_icon(coin, current_instance):
+	current_instance.texture = icons[coin.coin_id.coin_texture]
 	current_instance.custom_minimum_size = Vector2(64, 64)
 
 func set_path():
