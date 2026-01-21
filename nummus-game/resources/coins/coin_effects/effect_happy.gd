ba5d6441f8759578cfb2bc25e313606a99240a10
yaya.ny@proton.me <nianjindev@gmail.com>
2026-01-20 19:39:25 -0600

    test commit again

--- nummus-game/resources/coins/coin_effects/effect_happy.gd
+++ nummus-game/resources/coins/coin_effects/effect_happy.gd
@@ -9,7 +9,7 @@ func pre_effect(_stats: Dictionary):
 	pass
 func set_repeat():
 	repeat = 1
-func recurring(stats: Dictionary, state: int) -> Dictionary: # test
+func recurring(stats: Dictionary, state: int) -> Dictionary: # test commit 2
 	if state == Sides.HEADS:
 		CommonEffects.favor_heads(0.25)
 	return stats
