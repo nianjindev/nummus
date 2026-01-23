4af4eb5e4be1ed292e4f3869b5738faf9c60bf74
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2026-01-22 22:12:34 -0600

    fix: yes!

--- nummus-game/autoload/recursive_effect.gd
+++ nummus-game/autoload/recursive_effect.gd
@@ -2,21 +2,32 @@ extends Node
 
 var effects: Dictionary[Callable, int]
 
-var weight_effects: Dictionary[Callable, int]
+var weight_effects: Array[Object] = []
 
 func add_recurring_effect(function: Callable, period_length: int):
 	if function.get_object() == CommonEffects.WeightModifier:
-		weight_effects[function] = period_length
+		weight_effects.append(RecursiveEffectObject.new(function, period_length))
 	else:
 		effects[function] = period_length
 	print("The number of times I will recur is: " + str(period_length))
 
 func run_recurring_effect(stats: Dictionary, state: int) -> Dictionary:
-	for function in weight_effects.keys():
-		if weight_effects[function] == 0:
-			weight_effects.erase(function)
+	for effect in weight_effects:
+		if effect.period_length == 0:
+			weight_effects.erase(effect)
 			continue
 			
-		weight_effects[function] -= 1
-		function.call()
+		effect.period_length -= 1
+		effect.run()
 	return stats
+
+class RecursiveEffectObject:
+	var effect: Callable
+	var period_length: int
+	
+	func _init(given_effect: Callable, given_period_length: int):
+		effect = given_effect
+		period_length = given_period_length
+		
+	func run():
+		effect.call()
