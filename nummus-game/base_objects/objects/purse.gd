ed1c2cea3471b0f5f683c6559a53cee82268f40e
3M Arya <131737618+SLJ900-BRIDGE-GIRDER@users.noreply.github.com>
2026-01-23 21:12:34 -0600

    fix: inventory patch showing after hovering over it while input is locked ahahaha

--- nummus-game/base_objects/objects/purse.gd
+++ nummus-game/base_objects/objects/purse.gd
@@ -9,28 +9,31 @@ extends Node3D
 var is_mouse_over: bool = false
 
 func _ready() -> void:
-	hover_area.mouse_entered.connect(toggle_opened.bind(true))
-	hover_area.mouse_exited.connect(toggle_opened.bind(false))
+	hover_area.mouse_entered.connect(toggle_opened)
+	hover_area.mouse_exited.connect(toggle_opened)
 	_on_hover_area_mouse_exited()
 
-func toggle_opened(on: bool):
-	is_mouse_over = on
+func toggle_opened():
 	if Globals.input_locked:
 		await Signalbus.actions_finished
 		
-	opened_mesh.visible = on
-	closed_mesh.visible = not on
+	opened_mesh.visible = is_mouse_over
+	closed_mesh.visible = not is_mouse_over
 
 func _on_hover_area_mouse_entered() -> void:
+	is_mouse_over = true
+	
 	if Globals.input_locked:
 		await Signalbus.actions_finished
 	
-	if name == "PurseDiscard" and not Inventory.discard.is_empty(): #theres probably a better way to do this but idc
-		discard_ui.modulate = Color(1.0, 1.0, 1.0, 1.0) #has to be modulate and not hide or show
-	if name == "PurseInv" and not Inventory.current_inv.is_empty():
-		inventory_ui.modulate = Color(1.0, 1.0, 1.0, 1.0)
+	if is_mouse_over: #prevents patch from appearing after hovering over the purse while input is locked:
+		if name == "PurseDiscard" and not Inventory.discard.is_empty(): #theres probably a better way to do this but idc
+			discard_ui.modulate = Color(1.0, 1.0, 1.0, 1.0) #has to be modulate and not hide or show
+		if name == "PurseInv" and not Inventory.current_inv.is_empty():
+			inventory_ui.modulate = Color(1.0, 1.0, 1.0, 1.0)
 
 func _on_hover_area_mouse_exited() -> void:
+	is_mouse_over = false
 	
 	if name == "PurseDiscard":
 		discard_ui.modulate = Color(1.0, 1.0, 1.0, 0.0)
